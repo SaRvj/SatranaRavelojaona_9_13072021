9b3d0a3da296c8290d95bb6f47c0f352646b75c8
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-02-14 09:36:39 +0100

    modification action.js pour download

--- src/views/Actions.js
+++ src/views/Actions.js
@@ -7,11 +7,7 @@ export default (billUrl) => {
       <div id="eye" data-testid="icon-eye" data-bill-url=${billUrl}>
       ${eyeBlueIcon}
       </div>
-      <a href={myPDF} download="http://localhost:5678/public/4b392f446047ced066990b0627cfa444.pdf" data-bill-url=${billUrl}>
-      <div id="download" data-testid="icon-download" data-bill-url=${billUrl}>
-      ${downloadBlueIcon}
-      </div>
-      </a>
+      
     </div>`
   )
 }
